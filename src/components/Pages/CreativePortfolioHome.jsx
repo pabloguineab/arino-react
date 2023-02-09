8cede77321d6b6a9c38a14b21ce47bb2f33dbee8
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-09 14:55:19 -0500

    Update CreativePortfolioHome.jsx

--- src/components/Pages/CreativePortfolioHome.jsx
+++ src/components/Pages/CreativePortfolioHome.jsx
@@ -33,7 +33,7 @@ export default function CreativePortfolioHome() {
       subtitle: '.doc',
       btnText: 'Download Word File',
       btnLink: '/portfolio/portfolio-details',
-      imageUrl: '/images/portfolio35.png',
+      imageUrl: '/images/porfolio35.png',
       category: 'Editable File',
     },
     {
@@ -41,7 +41,7 @@ export default function CreativePortfolioHome() {
       subtitle: '.pdf',
       btnText: 'Download PDF File',
       btnLink: '/portfolio/portfolio-details',
-      imageUrl: '/images/portfolio36.png',
+      imageUrl: '/images/porfolio36.png',
       category: 'PDF File',
     },
     {
@@ -49,7 +49,7 @@ export default function CreativePortfolioHome() {
       subtitle: '.pdf',
       btnText: 'Downlaod Pagiarism Report',
       btnLink: '/portfolio/portfolio-details',
-      imageUrl: '/images/portfolio37.png',
+      imageUrl: '/images/porfolio37.png',
       category: 'Plagiarism Checked',
     },
     
