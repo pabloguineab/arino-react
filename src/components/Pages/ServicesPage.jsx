dbfdb4f75779268295b1a0d9853d3da5ee097291
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-09 10:13:20 -0800

    Update ServicesPage.jsx

--- src/components/Pages/ServicesPage.jsx
+++ src/components/Pages/ServicesPage.jsx
@@ -16,7 +16,7 @@ export default function ServicesPage() {
   }, [])
   return (
     <>
-      <Spacing lg='150' md='80'/>
+      <Spacing lg='50' md='80'/>
       <Div className='cs-shape_wrap_4'>
         <Div className="container">
       <Spacing lg='150' md='80'/>
