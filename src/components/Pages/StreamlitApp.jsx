31eac95b8a875e5ee8e02933e57bf4b66f9d28ea
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-17 15:51:05 -0800

    Add files via upload

---
+++ src/components/Pages/StreamlitApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function StreamlitApp() {
+    return (
+      <iframe
+        title="Streamlit App"
+        src="https://pabloguineab-genpdf-streamlit-app-o4rtfm.streamlit.app/"
+        width="100%"
+        height="800"
+      />
+    );
+  }
+  
+  export default StreamlitApp;
+
